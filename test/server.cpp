75ff2df3f7456a5e34e19f509b9e01d9866fb3e4
samurai0lava <ouhsseineilyass@gmail.com>
2025-07-09 12:01:51 +0100

    still learning

---
+++ test/server.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+
+int main()
+{
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if(serverSocket == -1)
+        return (-1);
+    
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    listen(serverSocket, 5);
+    
+    
+    //accepting the client messages here
+    int clientSocket = accept(serverSocket, nullptr, nullptr);
+    char buffer[1024] = { 0 };
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    std::cout << "Message from client: " << buffer << std::endl;
+    close(serverSocket);
+    return 0;
+}
