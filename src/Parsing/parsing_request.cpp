8f1c15504eba530322184f8fa18dbf4e0e546973
samurai0lava <ouhsseineilyass@gmail.com>
2025-09-10 14:04:40 +0100

    ADD

--- src/Parsing/parsing_request.cpp
+++ src/Parsing/parsing_request.cpp
@@ -716,7 +716,7 @@ ParsingRequest::ParseResult ParsingRequest::feed_data(const char* data, size_t l
 	// // cout << "CHUNK SIZE : " << len << endl;
 	// write(1, data, len);
 	// cout << "******END******\n";
-	buffer.append(data, len);
+	buffer.append(data, len);	
 	// refactor_data(buffer, data, len);	
 	// cout << "REFACTORED DATA : " << buffer << "XxXxXxXxXx\n" << endl;
 	while (current_state != PARSE_COMPLETE && current_state != PARSE_ERROR)
