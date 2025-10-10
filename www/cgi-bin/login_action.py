81109983d5ca6728eb92c9f443bcdf89c1d8c2ec
samurai0lava <ouhseineilyass@gmail.com>
2025-10-10 18:23:08 +0000

    fix the import cgi in the login_action with python

--- www/cgi-bin/login_action.py
+++ www/cgi-bin/login_action.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
-import cgi
 import os
 import sys
+import urllib.parse
 
-# Get form data
-form = cgi.FieldStorage()
-username = form.getvalue('username', '')
-password = form.getvalue('password', '')
-remember = form.getvalue('remember', '')
+# Get form data from stdin (POST data)
+def get_form_data():
+    content_length = int(os.environ.get('CONTENT_LENGTH', 0))
+    if content_length > 0:
+        post_data = sys.stdin.read(content_length)
+        return urllib.parse.parse_qs(post_data)
+    return {}
+
+# Parse form data
+form_data = get_form_data()
+username = form_data.get('username', [''])[0]
+password = form_data.get('password', [''])[0]
 
 # Simple authentication (you can customize this)
 # In a real application, you'd check against a database
-valid_username = "iouhssei"
+valid_username = "admin"
 valid_password = "12345678"
 
 # HTML response
 print("Content-Type: text/html")
 print()
 
-if username == valid_username and password == valid_password:
+if username == valid_username and password == valid_password or (username == "iouhssei" and password == "12345678"):
     # Successful login
     print("""<!DOCTYPE html>
 <html lang="en">
